8b3ad7250d71316e06acf06bb91ea8039e5984f9
manish <manishkeer530@gmail.com>
2025-08-20 12:30:08 +0530

    vercel 404 error fixed

--- src/GallaryImage.tsx
+++ src/GallaryImage.tsx
@@ -44,9 +44,9 @@ function GalleryPage() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 py-12">
+    <div className="min-h-screen bg-gray-50 ">
       <Navbar/>
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-12">
         <div className="text-center mb-12">
           <h1 className="text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
             School Gallery
